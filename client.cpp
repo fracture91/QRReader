9935c5c0882e7b548e219b9b5685a1901e89a9f2
Andrew Hurle <fracture91@gmail.com>
2011-10-30 21:14:03 -0400

    Initial commit.  Client sends message and server can read it.

---
+++ client.cpp
@@ -0,0 +1,82 @@
+//client
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <cstring>
+#include <unistd.h>
+#include <iostream>
+using namespace std;
+
+int sendFile(char *fileName, int fdSock);
+int rcvResponse(int fdSock);
+
+int main(int argc, char *argv[]) {
+	int status = 0;
+	int fdSock = 0;
+	struct addrinfo *addr = NULL;
+	//get command line args, check for valid
+	status = getaddrinfo("127.0.0.1", "2011", NULL, &addr);
+	if(status != 0) {
+		//print error msg
+		return -1;
+	}
+		
+	//connect to server
+	fdSock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+	if(fdSock == -1) {
+		//print error msg (errno)
+		return -1;
+	}
+	status = connect(fdSock, addr->ai_addr, addr->ai_addrlen);
+	if(status == -1) {
+		//print error msg (errno)
+		return -1;
+	}
+
+	//get file name
+	//send file
+	status = sendFile(NULL, fdSock);
+	if(status != 0) {
+	
+	}
+	cout << "client send status: " << status << endl;
+	
+	//receieve return code
+	//receive string
+		//get string array size
+		//get array
+	//if success:
+		//display URL
+	//if failure:
+		//display error message
+	//repeat or break out of loop
+	//disconnect
+	return status;
+}
+
+int sendFile(char *fileName, int fdSock) {
+	char buffer[128] = "Hello Server!";
+	int bufSize = strlen(buffer)+1;
+	int status = 0;
+	//open file
+	//read file into buffer
+	//get file size
+	//send file size to server
+	//send file to server
+	
+	
+	status = write(fdSock, buffer, bufSize);
+	if(status == -1) {
+		//print error msg (errno)
+		return -1;
+	}
+	else
+		status = 0;
+	
+	return status;
+}
+
+int rcvResponse(int fdSock) {
+	int status;
+	return status;
+}
---
+++ server.cpp
@@ -0,0 +1,88 @@
+//server
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <errno.h>
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+using namespace std;
+
+int writeToAdminLog(char *ipClient, char *message);
+
+int main(int argc, char *argv[]) {
+	int status = 0;
+	char port[16] = "2011";
+	int rateReqs = 0;
+	int rateSecs = 0;
+	int maxUsers = 0;
+	int timeOut = 0;
+	int fdSock = 0;
+	struct addrinfo hints, *res;
+	struct sockaddr_storage remoteAddr;
+	socklen_t remAddrLen = sizeof(remoteAddr);
+	int fdConn = 0;
+	int inBufSize = 128;
+	char *inputBuffer = new char[inBufSize];
+	
+	//fill in hints struct
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;  //don't care whether it's IPv4 or v6
+	hints.ai_socktype = SOCK_STREAM; //TCP
+	hints.ai_flags = AI_PASSIVE;
+	
+	//get command line args, check for valid
+	status = getaddrinfo(NULL, port, &hints, &res);
+	if(status != 0) {
+		cout << "getaddrinfo error: " << strerror(errno) << endl;
+		return -1;
+	}
+	
+	//open socket
+	fdSock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	cout << "fdSock: " << fdSock << endl;
+	
+	status = bind(fdSock, res->ai_addr, res->ai_addrlen); 
+	if(status != 0) {
+		cout << "bind error: " << strerror(errno) << endl;
+		return -1;
+	}
+	
+	
+	status = listen(fdSock, 10);
+	if(status != 0) {
+		cout << "listen error: " << strerror(errno) << endl;
+		return -1;
+	}
+	
+	cout << "server accepting" << endl;
+	fdConn = accept(fdSock, (struct sockaddr *)&remoteAddr, &remAddrLen);
+	cout << "server accepted" << endl;
+	cout << "remAddrLen: " << remAddrLen << endl;
+	cout << "fdConn value: " << fdConn << endl;
+	cout << "addr data: " << ((struct sockaddr_in6 *) &remoteAddr)->sin6_addr.s6_addr << endl;
+	status = read(fdConn, inputBuffer, inBufSize);
+	cout << "server read status: " << status << endl;
+	cout << "server read errmsg: " << strerror(errno) << endl;
+	cout << "server message: " << inputBuffer << endl;
+	//when receive new connection:
+		//check for too many users. If too many, don't fork
+		//fork
+		//original process waits for new connection
+}
+
+
+//new process:
+//check for rate limit violation or timeout
+//rcv client message
+	//check for file too large
+//call library to convert qr code to URL
+//determine return code
+//respond to client
+	//send return code to client
+		//convert to network byte order
+		//send number
+	//send string len to client
+		//convert to network byte order
+		//send number
+	//send string to client
