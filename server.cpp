21e5729c818318610c02f70c19d4acf71f61cd41
unknown <Eric@goat-desk.(none)>
2011-11-02 19:21:11 -0400

    Add ability to decode QR codes.

--- server.cpp
+++ server.cpp
@@ -1,26 +1,52 @@
 //server
 #define DEBUG 1
 
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <errno.h>
+#include <cstdlib>
 #include <cstring>
-#include <iostream>
 #include <netinet/in.h>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 #include "qrexception.h"
 
+/***********
+ * Defines *
+ ***********/
+#define RESULT_BUF_SIZE     512
+#define TMP_FILENAME_SIZE   64
+#define LIB_CALL_SIZE       128
+
+/********************
+ * Global Variables *
+ ********************/
+
+char g_port[16] = "2011";
+int g_rateReqs = 0;
+int g_rateSecs = 0;
+int g_maxUsers = 0;
+int g_timeOut = 0;
+
+//Function Prototypes
+
+//write a message to the admin log
 int writeToAdminLog(char *ipClient, char *message);
+
+//given an image and image size, give back a string contained in QR code
+int readQRCode(uint32_t imageSize, int8_t *image, string &result);
+
+int sendQRResult(string result);
+
+//Function definitions
 
 int main(int argc, char *argv[]) {
 	int status = 0;
-	char port[16] = "2011";
-	int rateReqs = 0;
-	int rateSecs = 0;
-	int maxUsers = 0;
-	int timeOut = 0;
 	int fdSock = 0;
 	struct addrinfo hints, *res;
 	struct sockaddr_storage remoteAddr;
@@ -30,7 +56,6 @@ int main(int argc, char *argv[]) {
 	char *buffer = new char[bufSize];
 	
 	try {
-		
 		//fill in hints struct
 		memset(&hints, 0, sizeof hints);
 		hints.ai_family = AF_UNSPEC;  //don't care whether it's IPv4 or v6
@@ -38,7 +63,7 @@ int main(int argc, char *argv[]) {
 		hints.ai_flags = AI_PASSIVE;
 		
 		//get command line args, check for valid
-		status = getaddrinfo(NULL, port, &hints, &res);
+		status = getaddrinfo(NULL, g_port, &hints, &res);
 		QRErrCheckNotZero(status, "getaddrinfo");
 		
 		//open socket
@@ -99,3 +124,45 @@ int main(int argc, char *argv[]) {
 		//convert to network byte order
 		//send number
 	//send string to client
+
+//TODO: error checking
+int readQRCode(uint32_t imageSize, int8_t *image, string &result) {
+	pid_t currPid;
+	streamsize count = 0;
+	char result_buffer[RESULT_BUF_SIZE] = "";
+	char image_filename[TMP_FILENAME_SIZE] = "";
+	char result_filename[TMP_FILENAME_SIZE] = "";
+	char sysCall[LIB_CALL_SIZE] = "java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner";
+	fstream tmpFile;
+	
+	//image filename will be pid (in hex) (each process only handles 1 image at a time)
+	currPid = getpid();
+	sprintf(image_filename, "%x", currPid);
+	sprintf(result_filename, "%x_result", currPid);
+	
+	//save image to file
+	tmpFile.open(image_filename, fstream::out | fstream::binary);
+	tmpFile.write((char*)image, (streamsize)imageSize);
+	tmpFile.close();
+	
+	//call library on file
+	//system call: "java [params] [image file] > [result file]"
+	sprintf(sysCall, "%s %s > %s", sysCall, image_filename, result_filename);
+	system(sysCall);
+	
+	//get result from result file
+	tmpFile.open(result_filename, fstream::in);
+	do {
+		tmpFile.read(result_buffer, RESULT_BUF_SIZE);
+		count = tmpFile.gcount();
+		//don't append if read nothing
+		if(count)
+			result.append(result_buffer, count);
+	} while(count == RESULT_BUF_SIZE); //repeat while buffer is completely full from last read
+	tmpFile.close();
+	
+	//delete temporary files
+	remove(image_filename);
+	remove(result_filename);
+	return 0;
+}
